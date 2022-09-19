3181139f337bf0f6086997abdfe824a31cf4548a
Mauricio Aguirre <mauricioaguirrert@yahoo.com>
2022-09-18 23:16:12 -0300

    Refactorizado con funcion

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,41 @@
 import  { Router} from 'express';
 import { USERS_BBDD } from "../bbdd.js"
+import authByEmailPwd from '../helpers/authByEmailPwd.js';
 
 
 const authRouter = Router();
 
-authRouter.get("publico", (req, res) => res.send("endpoint publico"));
+authRouter.get("/publico", (req, res) => res.send("endpoint publico"));
 
 
-authRouter.post("autenticado", (req, res) => {
+authRouter.post("/autenticado", (req, res) => {
     const {email, password} = req.body;
 
-    if(!email || !password) return res.send(400);
+    if(!email || !password) return res.sendStatus(400);
 
-    const user = USERS_BBDD.filter(user => user.email === email);
-    if(!user) return res.send(401)
+    try {
+        const user = authByEmailPwd(email, password);
+        return res.send(`Usuario ${user.name} autenticado`)
+    } catch (error) {
+        return res.sendStatus(401);
+    }
 
-    if(user.password !== password)
-    return res.send(401)
-
-    res.send(`Usuario ${user.name} autenticado`)
 });
 
-authRouter.post("autorizado", (req, res) => {
+authRouter.post("/autorizado", (req, res) => {
     const {email, password} = req.body;
 
-    if(!email || !password) return res.send(400);
+    if(!email || !password) return res.sendStatus(400);
 
-    const user = USERS_BBDD.filter(user => user.email === email);
-    if(!user) return res.send(401)
+    try {
+        const user = authByEmailPwd(email, password);
+        if(user.role !== 'admin') return res.sendStatus(403);
+        return res.send(`Usuario administrador ${user.name} autorizado`)
+    } catch (error) {
+        return res.sendStatus(401);
+    }
 
-    if(user.password !== password)
-    return res.send(401)
-
-    if(user.role !== 'admin') return res.send(403);
-
-    res.send(`Usuario administrador ${user.name} autorizado`)
+    
 })
 
 
